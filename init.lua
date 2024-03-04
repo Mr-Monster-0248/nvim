3d94389211b145003cdaad5185a98e8cbe6236df
Thibault LEPEZ <thibault.lepez@agregio-edf.com>
2024-03-04 15:47:05 +0100

    feat: change theme

--- init.lua
+++ init.lua
@@ -197,7 +197,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,18 +1,22 @@
--- return {
---   -- Theme inspired by Atom
---   'navarasu/onedark.nvim',
---   priority = 1000,
---   config = function()
---     vim.cmd.colorscheme 'onedark'
---   end,
--- }
+-- return
 
 
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin'
-  end
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 0,   -- change to 1000 when enable
+    enable = false, -- change to true to enable
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin'
+    end
+  }
 }
