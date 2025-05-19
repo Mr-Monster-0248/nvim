99fd03e679b124a572da8d26e87434b63a74df3f
Thibault LEPEZ <thibault.lepez@agregio-edf.com>
2025-05-19 11:02:21 +0200

    feat: update nvim config for 0.11

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,21 +1,33 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
   },
   config = function()
-    local harpoon = require("harpoon")
+    local harpoon = require 'harpoon'
     ---@diagnostic disable-next-line: missing-parameter
     harpoon:setup()
     local function map(lhs, rhs, opts)
-      vim.keymap.set("n", lhs, rhs, opts or {})
+      vim.keymap.set('n', lhs, rhs, opts or {})
     end
-    map("<leader>ha", function() harpoon:list():append() end, { desc = "%[H]arpoon %[A]dd to list" })
-    map("<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    map("<leader>1", function() harpoon:list():select(1) end, { desc = "Move to harpoon 1st buffer" })
-    map("<leader>2", function() harpoon:list():select(2) end, { desc = "Move to harpoon 2nd buffer" })
-    map("<leader>3", function() harpoon:list():select(3) end, { desc = "Move to harpoon 3rd buffer" })
-    map("<leader>4", function() harpoon:list():select(4) end, { desc = "Move to harpoon 4th buffer" })
-  end
+    map('<leader>ha', function()
+      harpoon:list():append()
+    end, { desc = '[H]arpoon [A]ppend file to list' })
+    map('<leader>hl', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+    map('<leader>1', function()
+      harpoon:list():select(1)
+    end)
+    map('<leader>2', function()
+      harpoon:list():select(2)
+    end)
+    map('<leader>3', function()
+      harpoon:list():select(3)
+    end)
+    map('<leader>4', function()
+      harpoon:list():select(4)
+    end)
+  end,
 }
--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,22 +1,8 @@
--- return
-
-
 return {
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 0,   -- change to 1000 when enable
-    enable = false, -- change to true to enable
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'catppuccin'
-    end
-  }
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'catppuccin'
+  end,
 }
